da0ccb76169a26de21eda185e02db5d516659277
George Prah II <georgepraho2@gmail.com>
2023-02-21 11:04:21 -0800

    write _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,9 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int main()
 {
-	char c[] = "_putchar"
+	char c[] = "_putchar";
 	write(1, c, 8);	
 	return 0;
 }

5ef2653735146e64649bd71216d578eead945b36
George Prah II <georgepraho2@gmail.com>
2023-02-21 14:04:48 -0800

    using ASCII

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		printf("%c", i);
+	}
+}

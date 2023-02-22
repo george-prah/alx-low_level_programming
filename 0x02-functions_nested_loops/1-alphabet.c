cc427fc5f635013d736e4d8233d4323c17861f12
george-prah <122888030+george-prah@users.noreply.github.com>
2023-02-22 11:07:04 +0000

    lower alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
 
 /**
  * print_alphabet - Prints the alphabet in lowercase.
@@ -22,6 +11,7 @@ void print_alphabet(void)
 
 	for (i = 97; i <= 122; i++)
 	{
-		printf("%c", i);
+		_putchar(i);
 	}
+	_putchar('\n');
 }

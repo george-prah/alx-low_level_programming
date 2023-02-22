df5784359ebc08a286352bd59b5325a99d56a232
george-prah <122888030+george-prah@users.noreply.github.com>
2023-02-22 20:08:07 +0000

    _putchar(_putchar)

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,17 @@
 /**
  * main - Prints _putchar as a message.
  *
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char x[] = "_putchar";
-	int	i;
-	
-	for (i = 0; i <= 7; i++)
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar(x[i]);
+		_putchar(str[count]);
 	}
 	_putchar('\n');
 	return (0);

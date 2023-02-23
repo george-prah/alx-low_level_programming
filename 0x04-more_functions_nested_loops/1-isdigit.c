81804c3d0e96e03a3c301aad1f3db4ba0074d4e7
george-prah <122888030+george-prah@users.noreply.github.com>
2023-02-23 19:04:18 +0000

    digit check 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * digit check.
+ *Return:1 for success, 0 for otherwise.
+ */
+ int _isdigit(int c)
+ {
+   if (c >= 48 && c <= 57)
+   {
+     return (1);
+   }
+   else
+   {
+     return (0);
+   }
+  
+ }

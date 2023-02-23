c1b455ee91930c3c5d2831df73e95e64832b9bf9
george-prah <122888030+george-prah@users.noreply.github.com>
2023-02-23 18:26:17 +0000

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * main - checks for upper case
-  *
-  * Return: 1 for success, 0 for otherwise
-  */
+ * main - checks for upper case
+ *
+ * Return: 1 for success, 0 for otherwise
+ */
 int _isupper(int c)
 {
   if (c > 64 && c < 91)

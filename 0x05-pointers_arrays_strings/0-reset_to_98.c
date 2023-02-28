8e00eb0fb2124ddb886d44293ae34dbe7a37f521
george-prah <122888030+george-prah@users.noreply.github.com>
2023-02-28 17:45:45 +0000

    update int pointed to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - takes pointer to int
+ * takes pointer to a an int and updates the value it points to to 98
+ *
+ * Return: 98 
+ */
+ void reset_to_98(int *n)
+ {
+ int n;
+ int *p = &n;
+ *p = 98;
+ return (n);
+ }
